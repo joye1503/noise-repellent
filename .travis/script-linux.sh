a456dba43a76c177c8311e797658803db0c7b7f1
Luciano Dato <lucianodato@gmail.com>
2019-05-01 18:13:49 -0300

    Added travis cross compiling

---
+++ .travis/script-linux.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+# Preparation
+_FLAGS="-Werror"
+export CFLAGS="${_FLAGS}"
+export CXXFLAGS="${_FLAGS}"
+
+# Start clean
+make clean >/dev/null
+
+# Build now
+make
+
+# Compress build in a zip file
+zip -9 -r noise-repellent-$TRAVIS_TAG-linux.zip ./bin
